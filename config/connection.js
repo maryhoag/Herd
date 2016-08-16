e74d81c7848f5cafa0cfbdbd1c844d8a395b81fa
maryhoag <marymcevoyhoag@gmail.com>
2016-08-16 15:11:33 -0400

    updated

---
+++ config/connection.js
@@ -0,0 +1,35 @@
+// require mysql 
+var mysql = require('mysql');
+
+// retrieving the username and password for the db connection
+// var username = process.argv[2];
+// var password = process.argv[3];
+
+//creating the connection to the db
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+//sql local connection
+// mysql.createConnection({
+// 	host: 'localhost',
+// 	user: username,
+// 	password: password,
+// 	database: 'carPool'
+// });
+
+
+//jaws DB connection
+// mysql.createConnection(process.env.JAWSDB_URL);
+
+// if fail to connect display the error otherwise if successful give the connection id
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+
+
+module.exports = connection;
+
+
---
+++ server.js
@@ -0,0 +1,67 @@
+// requires express and body-parser
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var expressSessions = require('express-session');
+var passport = require('passport');
+var passportLocal = require('passport-local');
+ 
+
+
+// creating an instance of express
+var app = express();
+var PORT = process.env.PORT || 3000; // assigning the port or using the PORT environment variable
+
+
+// makes static content in assets accessible
+app.use(express.static(process.cwd() + '/assets'));	
+
+
+// require orm - mysql queries
+var orm = require('./config/orm.js');
+
+// BodyParser interprets data sent to the server
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+
+
+//setting up handlebars
+var exphbs = require('express-handlebars');
+var hbs = require('handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(cookieParser());
+app.use(expressSessions({secret: 'secret'}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new passportLocal.Strategy({passReqToCallback : true}, function(req, username, password, done) {
+	console.log(username,password)
+	
+	orm.findUser(req, username, password, done);	
+}))
+
+passport.serializeUser(function(user,done){
+	done(null, user);
+})
+
+passport.deserializeUser(function(user,done){
+	done(null, user);
+})
+
+
+//require routes
+require('./routing/html-routes.js')(app);
+
+
+
+//starts the server letting user know the PORT
+app.listen(PORT, function(){
+
+	console.log("listening on port %d", PORT);
+});
